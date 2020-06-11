aa2aa7948ac9b4f66531b364183bcca8908d2fdf
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-06-11 01:21:21 -0500

    Idea general del blog en index.php

---
+++ public/Blog/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Blog</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    </head>
+    <body>
+        <h1>Titulo del blog</h1>
+        <p>Resumen del contenido del que trata este blog.</p>
+
+        <h2>Titulo del articulo 1</h2>
+        <div>día mes año</div>
+        <p>Un párrafo resumiendo el articulo 1.</p>
+        <p>
+            <a href="#">Leer más...</a>
+        </p>
+
+        <h2>Titulo del articulo 2</h2>
+        <div>11 de junio de 2020</div>
+        <p>Un párrafo resumiendo el articulo 2.</p>
+        <p>
+            <a href="#">Leer más...</a>
+        </p>
+    </body>
+</html>
