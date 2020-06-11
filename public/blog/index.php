097b46b0b206b9d452f7897137cf5b94a8d61a04
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-06-11 03:25:42 -0500

    Update index.php

--- public/blog/index.php
+++ public/blog/index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Blog</title>
-        <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
-    </head>
-    <body>
-        <h1>Titulo del blog</h1>
-        <p>Resumen del contenido del que trata este blog.</p>
+<head>
+    <title>Blog</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+</head>
+<body>
+    <h1>Titulo del blog</h1>
+    <p>Resumen del contenido del que trata este blog.</p>
 
-        <?php for ($postId = 1; $postId <=3; $postId++): ?>
-            <h2>Titulo del articulo <?php echo $postId ?></h2>
-            <div>día mes año</div>
-            <p>Un párrafo resumiendo el articulo <?php echo $postId ?>.</p>
-            <p>
-                <a href="#">Leer más...</a>
-            </p>
-        <?php endfor ?>
+    <?php for ($postId = 1; $postId <=3; $postId++): ?>
+        <h2>Titulo del articulo <?php echo $postId ?></h2>
+        <div>día mes año</div>
+        <p>Un párrafo resumiendo el articulo <?php echo $postId ?>.</p>
+        <p>
+            <a href="#">Leer más...</a>
+        </p>
+    <?php endfor ?>
 
-    </body>
+</body>
 </html>
