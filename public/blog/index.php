b08627154d51f274467a10325c8f33c6002e4ea6
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-06-11 21:15:32 -0500

    Se actualizó index.php para mostrar posts de la base de datos

--- public/blog/index.php
+++ public/blog/index.php
@@ -1,21 +1,41 @@
+<?php
+// Ruta a la base de datos, para que SQLite / PDO pueda conectarse
+$root = __DIR__;
+$database = $root . '/data/data.sqlite';
+$dsn = 'sqlite:' . $database;
+
+// Se conecta a la base de datos, ejecuta una consulta, maneja errores
+$pdo = new PDO($dsn);
+$stmt = $pdo->query('SELECT titulo, fecha_creacion, cuerpo
+        FROM post ORDER BY fecha_creacion DESC');
+if ($stmt === false){
+    throw new Exception('Hubo un problema al ejecutar este query');
+}
+?>
+
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Blog</title>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
-</head>
-<body>
+    <head>
+        <title>Blog</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    </head>
+    <body>
     <h1>Titulo del blog</h1>
     <p>Resumen del contenido del que trata este blog.</p>
 
-    <?php for ($postId = 1; $postId <=3; $postId++): ?>
-        <h2>Titulo del articulo <?php echo $postId ?></h2>
-        <div>día mes año</div>
-        <p>Un párrafo resumiendo el articulo <?php echo $postId ?>.</p>
+    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+        <h2>
+            <?php echo htmlspecialchars($row['titulo'], ENT_HTML5,'UTF-8')?>
+        </h2>
+        <div>
+            <?php echo $row['fecha_creacion'] ?>
+        </div>
+        <p>
+            <?php echo htmlspecialchars($row['cuerpo'], ENT_HTML5, 'UTF-8')?>
+        </p>
         <p>
             <a href="#">Leer más...</a>
         </p>
-    <?php endfor ?>
-
-</body>
+    <?php endwhile; ?>
+    </body>
 </html>
