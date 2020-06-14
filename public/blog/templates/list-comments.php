c6cc591162731688299d6aa89f26f74689049c86
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-06-14 02:33:11 -0500

    Listado de comentarios y funcion para que el admin los elimine

---
+++ public/blog/templates/list-comments.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @var $pdo PDO
+ * @var $postId integer
+ */
+?>
+<form
+    action="view-post.php?action=delete-comment&amp;post_id=<?php echo $postId?>&amp;"
+    method="post"
+    class="comment-list"
+>
+    <h3><?php echo countComments($pdo, $postId) ?> comentarios</h3>
+    <?php foreach (getComments($pdo, $postId) as $comment): ?>
+        <div class="comment">
+            <div class="comment-meta">
+                Comentario de
+                <?php echo htmlSpecial($comment['nombre']) ?>
+                el
+                <?php echo convertSqlDate($comment['fecha_creacion']) ?>
+                <?php if (isLoggedIn()): ?>
+                    <input
+                        type="submit"
+                        name="delete-comment[<?php echo $comment['id'] ?>]"
+                        value="Eliminar"
+                    />
+                <?php endif ?>
+            </div>
+            <div class="comment-body">
+                <?php echo convertToParagraphs($comment['texto']) ?>
+            </div>
+        </div>
+    <?php endforeach ?>
+</form>
